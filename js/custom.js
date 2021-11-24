8b05dbe18e11089a91fa629699384212848a30fb
Samina-Chowdhury <saminachowdhury1003@gmail.com>
2021-11-24 10:52:50 +0600

    file ready upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.banner-slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        slidesToShow: 1, 
+    });
+    //about video popup,
+    $('.venobox').venobox();
+});
